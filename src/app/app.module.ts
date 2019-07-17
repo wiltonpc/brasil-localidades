4b80b934ca9ed0e79a849802ab12949566b50b0c
Wilton Pereira <wiltonpc@gmail.com>
2019-07-17 17:00:31 -0300

    component estados

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EstadosComponent } from './estados/estados.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EstadosComponent
   ],
   imports: [
     BrowserModule,
